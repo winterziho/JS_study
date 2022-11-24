69ed6180fe719b9c0f5842acc6220645e686cb78
winterziho <winterziho@gmail.com>
2022-11-24 19:05:23 +0900

    else if

---
+++ if/elseif.js
@@ -0,0 +1,16 @@
+// if문 (if statement)
+
+let temperature = -150;
+
+if (temperature <= 0) {  //조건부분
+    console.log('물이 업니다.') //동작부분
+} else if(temperature < 100){
+    console.log('물이 얼지도 끓지도 않습니다.');
+} else if(temperature < 150) {
+    console.log('물이 끓습니다.');
+} else {
+    console.log('물이 모두 수증기가 되었습니다.')
+}
+
+
+
